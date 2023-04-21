f9941fd5917dbf64ae035962474f2ece409a26dd
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-21 17:09:25 +0300

    create getTag and getTags methods for Scraper class

--- peviitor_scraper.js
+++ peviitor_scraper.js
@@ -19,6 +19,16 @@ class Scraper {
         return new jssoup(response.data);
     }
 
+    async getTag(tag, attrs){
+        const response = await this.soup;
+        return response.find(tag, attrs);
+    }
+
+    async getTags(tag, attrs){
+        const response = await this.soup;
+        return response.findAll(tag, attrs);
+    }
+
 }
 
 
