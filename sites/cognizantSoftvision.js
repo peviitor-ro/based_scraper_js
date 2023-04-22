0ba7cd8cba0f8ff4003990b90db73d6da9b8187c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-22 14:28:37 +0300

    create scraper for cognizantsoftvision

---
+++ sites/cognizantSoftvision.js
@@ -0,0 +1,57 @@
+"use strict"
+
+const scraper = require(".././peviitor_scraper.js");
+const axios = require('axios');
+const uuid = require('uuid');
+
+const url = "https://www.cognizantsoftvision.com/job-search/?location=romania";
+
+const s = new scraper.Scraper(url);
+
+let finalJobs = [];
+const company = {"company":"CognizantSoftvision"};
+
+s.soup.then((soup) => {
+    const json = JSON.parse(soup.find("script", {"type":"application/json"}).text);
+
+    json.props.pageProps.jobOpenings.jobs.forEach((job) => {
+        if (job.location == "Romania") {
+            const id = uuid.v4();
+            const job_title = job.title;
+            const job_link = "https://www.cognizantsoftvision.com" + job.link;
+            const company = "CognizantSoftvision";
+            const city = job.location;
+            const country = "Romania";
+
+            console.log(job_title + " -> " + city);
+
+            const j = {
+                "id": id,
+                "job_title": job_title,
+                "job_link": job_link,
+                "company": company,
+                "city": city,
+                "country": country
+            }
+
+            finalJobs.push(j);
+        }
+    })
+}).then(() => {
+    console.log("Total jobs: " + finalJobs.length);
+    const apiKey = "182b157-bb68-e3c5-5146-5f27dcd7a4c8";
+    axios.post("https://api.peviitor.ro/v4/clean/", company, {
+        headers: {
+            'apikey': apiKey,
+            "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }
+    );
+    axios.post("https://api.peviitor.ro/v4/update/", JSON.stringify(finalJobs) , {
+        headers: {
+            'apikey': apiKey,
+            "Content-Type": "aplication/json"
+            }
+        }
+    );
+})
