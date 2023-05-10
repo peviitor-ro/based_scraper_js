c002608ba78d895d46fce15dab47aa40f8779a1c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-10 18:13:40 +0300

    create scraper for hilti

---
+++ sites/hilti.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const scraper = require("../peviitor_scraper.js");
+const uuid = require("uuid");
+
+const url =
+  "https://careers.hilti.group/ro/locuri-de-munca/?search=&country=20000441&pagesize=100#results";
+
+const company = { company: "Hilti" };
+let finalJobs = [];
+
+const s = new scraper.Scraper(url);
+
+s.soup
+  .then((response) => {
+    const jobs = response.findAll("div", { class: "card-job" });
+
+    jobs.forEach((job) => {
+      const id = uuid.v4();
+      const job_title = job.find("a").text.trim();
+      const job_link = "https://careers.hilti.group" + job.find("a").attrs.href;
+      const country = "Romania";
+      const city = job
+        .find("li", { class: "list-inline-item" })
+        .text.split(",")[0]
+        .trim();
+
+      console.log(job_title + " -> " + city);
+
+      finalJobs.push({
+        id: id,
+        job_title: job_title,
+        job_link: job_link,
+        company: company.company,
+        country: country,
+        city: city,
+      });
+    });
+  })
+  .then(() => {
+    console.log("Total jobs: " + finalJobs.length);
+
+    const apiKey = "182b157-bb68-e3c5-5146-5f27dcd7a4c8";
+    const postPeviitor = scraper.postApiPeViitor(apiKey, finalJobs, company);
+
+    let logo = "https://careers.hilti.group/images/logo.svg";
+
+    let postLogo = new scraper.ApiScraper(
+      "https://api.peviitor.ro/v1/logo/add/"
+    );
+    postLogo.headers.headers["Content-Type"] = "application/json";
+    postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
+  });
