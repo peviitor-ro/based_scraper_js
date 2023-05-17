82320b689f119e7fcb94bcd3d0d25d93f7b81d20
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-17 13:18:16 +0300

    create scraper for brinks

---
+++ sites/brinks.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const scraper = require("../peviitor_scraper.js");
+const uuid = require("uuid");
+
+const url = "https://ro.brinks.com/ro/careers/career-positions";
+
+const company = { company: "Brinks" };
+let finalJobs = [];
+
+const s = new scraper.Scraper(url);
+
+s.soup
+  .then((soup) => {
+    const jobs = soup.findAll("div", { class: "card" });
+
+    jobs.forEach((job) => {
+      const id = uuid.v4();
+      const job_title = job.find("h2").text.trim();
+      const job_link = job.find("a").attrs.href;
+      const country = "Romania";
+      const city = "Romania";
+
+      console.log(job_title + " -> " + city);
+
+      finalJobs.push({
+        id: id,
+        job_title: job_title,
+        job_link: job_link,
+        country: country,
+        city: city,
+        company: company.company,
+      });
+    });
+  })
+  .then(() => {
+    console.log("Total jobs: " + finalJobs.length);
+
+    const apiKey = "182b157-bb68-e3c5-5146-5f27dcd7a4c8";
+    const postPeviitor = scraper.postApiPeViitor(apiKey, finalJobs, company);
+
+    let logo =
+      "https://ro.brinks.com/o/brinks-website-theme/images/logos/brinks/brinks-logo-blue.png";
+
+    let postLogo = new scraper.ApiScraper(
+      "https://api.peviitor.ro/v1/logo/add/"
+    );
+    postLogo.headers.headers["Content-Type"] = "application/json";
+    postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
+  });
