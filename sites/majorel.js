3e5f4f19bc8ed91f1f2d3f16fac9fbf5b2005eb1
MarcelDRM <marin.marcel89@gmail.com>
2023-07-08 23:54:45 +0300

    final try

--- sites/majorel.js
+++ sites/majorel.js
@@ -2,7 +2,8 @@
 const scraper = require("../peviitor_scraper.js");
 const uuid = require("uuid");
 
-const url = "https://jobs.majorel.com/romania/en/jobs?lang=en&api=jobs&site=28&_";
+const url =
+  "https://jobs.majorel.com/romania/en/jobs?lang=en&api=jobs&site=28&_";
 
 const company = { company: "Majorel" };
 let finalJobs = [];
@@ -25,55 +26,35 @@ async function data() {
 
   do {
     fetchedJobs = await fetchData(pages);
-    jobs = jobs.concat(fetchedJobs);
+    fetchedJobs.forEach((element) => {
+      jobs.push(element);
+    });
     pages++;
   } while (fetchedJobs.length > 0);
 
-  console.log(jobs.length);
-  return jobs;
+  jobs.forEach((job) => {
+    const id = uuid.v4();
+    const job_title = job.name;
+    const job_link = job.url;
+    let city = "Romania";
+
+    if (job.location != null) {
+      if (job.location.name != "") {
+        city = job.location.name;
+      }
+    }
+
+    finalJobs.push({
+      id: id,
+      job_title: job_title,
+      job_link: job_link,
+      company: company.company,
+      city: city,
+      country: "Romania",
+    });
+  });
+
+  console.log(JSON.stringify(finalJobs, null, 2));
 }
 
 data();
-
-
-
-// const s = new scraper.ApiScraper(url)
-// s.get().then((response)=>{
-
-//     const jobs = response
-//     console.log(jobs.length)
-
-//     jobs.forEach(job => {
-
-//         const id = uuid.v4();
-//       const job_title = job.name
-//       const job_link = job.url
-//       let city = "Romania"
-
-//          if(job.location != null){
-//              if(job.location.name != ""){
-//                  city = job.location.name
-//              }
-                    
-//             } 
-
-//         //console.log(job.location)
-
-
-
-//       finalJobs.push({
-//         id: id,
-//         job_title: job_title,
-//         job_link: job_link,
-//         company: company.company,
-//         city: city,
-//         country: "Romania",
-//       })
-        
-//     });
-
-// }).then(() =>{
-//     // console.log(finalJobs.length)
-//     // console.log(JSON.stringify(finalJobs, null, 2))
-// })
-
