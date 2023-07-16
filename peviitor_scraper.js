37f3e2ae825826b4dd2991011292873f3a88bd5b
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-17 00:46:10 +0300

    fixed eaton

--- peviitor_scraper.js
+++ peviitor_scraper.js
@@ -44,21 +44,23 @@ class ApiScraper {
   }
 }
 
-function postApiPeViitor(data, company) {
+function postApiPeViitor(data, company, apikey = null) {
   const V4cleanUrl = "https://api.peviitor.ro/v4/clean/";
   const V1cleanUrl = "https://api.peviitor.ro/v1/clean/";
   const V4updateUrl = "https://api.peviitor.ro/v4/update/";
   const V1updateUrl = "https://api.peviitor.ro/v1/update/";
 
-  const apikey = process.env.APIKEY;
+  if (apikey == null) {
+    apikey = process.env.APIKEY;
+  } 
 
   const scraper = new ApiScraper();
   let resolveApi = "https://dev.laurentiumarian.ro/scraper/based_scraper_js/";
-  let status = { status: company.company + ".js/" };
+  let status = { status: company.company.toLowerCase() + ".js" };
   axios
     .post(resolveApi, JSON.stringify(status))
     .then((response) => {
-      if (response.status == "active") {
+      // if (response.data == "active") {
         scraper.url = V4cleanUrl;
         scraper.headers.headers["Content-Type"] =
           "application/x-www-form-urlencoded";
@@ -70,29 +72,29 @@ function postApiPeViitor(data, company) {
 
           scraper.post(JSON.stringify(data));
         });
-      } else {
-        scraper.url = V1cleanUrl;
-        scraper.headers.headers["Content-Type"] =
-          "application/x-www-form-urlencoded";
-        scraper.headers.headers["apikey"] = apikey;
+      // } else {
+      //   scraper.url = V1cleanUrl;
+      //   scraper.headers.headers["Content-Type"] =
+      //     "application/x-www-form-urlencoded";
+      //   scraper.headers.headers["apikey"] = apikey;
 
-        scraper
-          .post(company)
-          .then(() => {
-            scraper.url = V1updateUrl;
-            scraper.headers.headers["Content-Type"] = "application/json";
+      //   scraper
+      //     .post(company)
+      //     .then(() => {
+      //       scraper.url = V1updateUrl;
+      //       scraper.headers.headers["Content-Type"] = "application/json";
 
-            scraper.post(JSON.stringify(data));
-          })
-          .then(() => {
-            scraper.url = V4cleanUrl;
-            scraper.headers.headers["Content-Type"] =
-              "application/x-www-form-urlencoded";
-            scraper.headers.headers["apikey"] = apikey;
+      //       scraper.post(JSON.stringify(data));
+      //     })
+      //     .then(() => {
+      //       scraper.url = V4cleanUrl;
+      //       scraper.headers.headers["Content-Type"] =
+      //         "application/x-www-form-urlencoded";
+      //       scraper.headers.headers["apikey"] = apikey;
 
-            scraper.post(company);
-          });
-      }
+      //       scraper.post(company);
+      //     });
+      // }
     })
     .catch((error) => {
       console.log("Error sending trigger for " + file);
