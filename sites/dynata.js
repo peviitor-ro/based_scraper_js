91a5f0ba43b08c76c82cdfc8a7533598c06b38e4
Codetamer333 <kattalynho_no1@yahoo.com>
2023-07-16 23:28:57 +0300

    3firms

--- sites/dynata.js
+++ sites/dynata.js
@@ -13,31 +13,12 @@ fetch(url, {
   method: 'POST',
   body: JSON.stringify(requestBody),
   headers: { 
-    'Accept': 'application/json',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US',
-    'Connection': 'keep-alive',
-    'Content-Length': '141',
     'Content-Type':'application/json',
-    'Cookie': 'wday_vps_cookie=3391531530.3635.0000; timezoneOffset=-180; _ga=GA1.4.109040793.1688503556; PLAY_SESSION=125528c863b3d2d69e977b456316cdf664f0f421-dynata_pSessionId=j2bi6v495afti6oveg9bilr6cf&instance=wd1prvps0001g; TS014c1515=01dc4a3ac88ebae27803a55d336307b83f7a3f779357a9d49184256d1613ee4afbd469e7934bc0f7fc4a4ed8fc2996f2bbd2bfc62c; _gat=1; wd-browser-id=e3358515-e402-4498-af7a-cc5c1cc62cde; CALYPSO_CSRF_TOKEN=8009e3f7-5d7f-49cc-bddf-0f1156b09507',
-    'Dnt': '1',
-    'Host': 'dynata.wd1.myworkdayjobs.com',
-    'Origin': 'https://dynata.wd1.myworkdayjobs.com',
-    'Referer': 'https://dynata.wd1.myworkdayjobs.com/careers?locations=67cdbb242c0f01186560ab7ce9361603&locations=67cdbb242c0f01ff4f8eac7ce9361d03',
-    'Sec-Ch-Ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': 'Windows',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-    'X-Calypso-Csrf-Token': '8009e3f7-5d7f-49cc-bddf-0f1156b09507'
   }
 })
   .then(response => response.json())
   .then(responseData => {
-    delete responseData.facets;
-    delete responseData.userAuthenticated;
      const jobPostings = responseData.jobPostings;
      jobPostings.forEach(job => {
        const id = uuid.v4();
@@ -48,7 +29,7 @@ fetch(url, {
         job_title: jobTitle,
         job_link: externalPath,
         country: "Romania",
-        city: "",
+        city: "Romania",
         company: company.company,
       });
      });
@@ -68,10 +49,7 @@ fetch(url, {
     postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
 
    })
-   .catch(error => {
-     console.log(error);
-   });
-
+  
 
 
 
--- sites/pentasia.js
+++ sites/pentasia.js
@@ -26,20 +26,7 @@ const fetchJobs = (offset) => {
     method: 'POST',
     body: JSON.stringify(requestBody),
     headers: {
-      'authority': 'www.pentasia.com', 
-      'accept': '*/*' ,
-      'accept-language': 'ro-RO,ro;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,fr;q=0.5,it;q=0.4,pt;q=0.3,tr;q=0.2,es;q=0.1,pl;q=0.1,la;q=0.1',
-      'content-type': 'application/json', 
-      'cookie': '_ga=GA1.1.1363648337.1689058080; _ga_8PCZNQ7K62=GS1.1.1689060395.2.1.1689061342.0.0.0', 
-      'dnt': '1', 
-      'origin': 'https://www.pentasia.com',
-      'referer': 'https://www.pentasia.com/jobs/', 
-      'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"', 
-      'sec-ch-ua-mobile': '?0', 
-      'sec-ch-ua-platform':'"Windows"',
-      'sec-fetch-dest': 'empty', 
-      'sec-fetch-mode': 'cors', 
-      'sec-fetch-site': 'same-origin', 
+      'content-type': 'application/json',  
       'user-agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' 
     }
   }).then(response => response.json());
@@ -56,22 +43,6 @@ Promise.all([request1, request2, request3])
     const concatenatedData = responseDataArray.reduce((result, responseData) => {
       return result.concat(responseData.results);
     }, []);
-         concatenatedData.forEach(item => {
-         delete item.job.id;
-         delete item.job.language_code;
-         delete item.job.salary_package;
-         delete item.job.created_at;
-         delete item.job.published_at;
-         delete item.job.updated_at;
-         delete item.job.expires_at;
-         delete item.job.featured;
-         delete item.title_snippet;
-         delete item.job.derived_info.job_categories;
-         delete item.job.derived_info.locations;
-         delete item.text_snippet;
-         delete item.commute_info;
-         delete item.job.categories;
-         })
          const filteredResults = concatenatedData.filter(obj => {
            return obj.job.addresses.toString().includes('Romania') || obj.summary.includes('Romania') || obj.job.url_slug.includes("Romania") || obj.job.description.includes("Romania")  
          });
@@ -86,11 +57,10 @@ Promise.all([request1, request2, request3])
              job_title: jobTitle,
              job_link: externalPath,
              country: "Romania",
-             city: "",
+             city: "Romania",
              company: company.company,
            });
-     
-         })     
+         })    
   })
  .then(() => {
          console.log(JSON.stringify(finalJobs, null, 2));
@@ -107,9 +77,7 @@ Promise.all([request1, request2, request3])
          postLogo.post(JSON.stringify([{ id: company.company, logo: logo }]));
      
   })
-  .catch(error => {
-    console.log(error);
-  });
+ 
 
 
 
