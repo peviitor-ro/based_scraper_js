370cae19da37184be910af2390952b7cb2502ac3
Baluta Laurentiu Marian <laurentiumarianbaluta@gmail.com>
2023-07-18 10:21:40 +0000

    remove all console logs

--- sites/brightantity.js
+++ sites/brightantity.js
@@ -15,15 +15,11 @@ Promise.all([s.soup, s2.soup])
     .then(([soup, soup2]) => {
         const jobsS = Object.values(soup.findAll("h2", { class: "eael-entry-title" }));
         const jobsS2 = Object.values(soup2.findAll("h2", { class: "eael-entry-title" }));
-        console.log("Type of jobsS:", typeof jobsS);
-        console.log("Type of jobsS2:", typeof jobsS2);
         const jobs = [...jobsS, ...jobsS2];
         jobs.forEach((job) => {
             const id = uuid.v4();
             const job_title = job.find("a").text.trim();
-            console.log(job_title);
             const job_link = job.find("a").attrs.href;
-            console.log(job_link);
             finalJobs.push({
                 id: id,
                 job_title: job_title,
