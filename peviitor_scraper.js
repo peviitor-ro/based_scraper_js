3839527efc6d76d421a6157c0cb7cfc34a8475ad
Costin <virlan.costin.andrei@gmail.com>
2023-07-19 05:44:38 +0300

    Now using templates

--- peviitor_scraper.js
+++ peviitor_scraper.js
@@ -2,7 +2,7 @@
 
 const axios = require("axios");
 const jssoup = require("jssoup").default;
-const {Scraper, ApiScraper} = require("./lib");
+const { Scraper, ApiScraper } = require("./lib");
 
 function postApiPeViitor(data, company, apikey = null) {
   const V4cleanUrl = "https://api.peviitor.ro/v4/clean/";
@@ -10,25 +10,25 @@ function postApiPeViitor(data, company, apikey = null) {
 
   const scraper = new ApiScraper();
   const resolveApi = "https://dev.laurentiumarian.ro/scraper/based_scraper_js/";
-  const status = { status: company.company.toLowerCase() + ".js" };
+  const status = { status: `${company.company.toLowerCase()}.js` };
   axios
     .post(resolveApi, JSON.stringify(status))
     .then((response) => {
       // if (response.data == "active") {
-        scraper.url = V4cleanUrl;
-        scraper.headers.headers["Content-Type"] =
-          "application/x-www-form-urlencoded";
-        scraper.headers.headers.apikey = apikey || process.env.APIKEY;
+      scraper.url = V4cleanUrl;
+      scraper.headers.headers["Content-Type"] =
+        "application/x-www-form-urlencoded";
+      scraper.headers.headers.apikey = apikey || process.env.APIKEY;
 
-        scraper.post(company).then(() => {
-          scraper.url = V4updateUrl;
-          scraper.headers.headers["Content-Type"] = "application/json";
+      scraper.post(company).then(() => {
+        scraper.url = V4updateUrl;
+        scraper.headers.headers["Content-Type"] = "application/json";
 
-          scraper.post(JSON.stringify(data));
-        });
+        scraper.post(JSON.stringify(data));
+      });
     })
     .catch((error) => {
-      console.log("Error sending trigger for " + file);
+      console.log(`Error sending trigger for ${file}`);
       console.log(error);
     });
 }
