753d5a2c07aeafa01fb7ea77be957c7b6fb02dd8
Codetamer333 <kattalynho_no1@yahoo.com>
2023-07-21 10:55:12 +0300

    add fildas

--- sites/fildas.js
+++ sites/fildas.js
@@ -2,9 +2,9 @@
 const scraper = require("../peviitor_scraper.js");
 const uuid = require("uuid");
 
-const url = "https://www.ameropa.ro/ro/joburi-disponibile/";
+const url = "https://www.fildas.ro/cariere/";
 
-const company = { company: "ameropa" };
+const company = { company: "fildas" };
 let finalJobs = [];
 const SUFFIX = "#:~:text=";
 const apiKey = process.env.KNOX
@@ -12,31 +12,29 @@ const s = new scraper.Scraper(url);
 
 s.soup
   .then((soup) => {
-    const jobs = soup.findAll("a", { class: "elementor-accordion-title" });
-    console.log(jobs.length);
+    const jobs = soup.findAll("h2");
     jobs.forEach((job) => {
       const id = uuid.v4();
-      const job_title = job.text.trim();
-      console.log(job_title);   
+      const job_title = job.text.trim().split("&#8211;")[0];
       const job_link = url + SUFFIX + job_title;
-      console.log(job_link);
-
+      const city = job.text.trim().split("&#8211;")[1]
       finalJobs.push({
         id: id,
         job_title: job_title,
         job_link: job_link,
         company: company.company,
-        city: "Constanta",
+        city: city,
         country: "Romania",
       });
     });
+    console.log(finalJobs.length);
   })
   .then(() => {
     console.log(JSON.stringify(finalJobs, null, 2));
     scraper.postApiPeViitor(finalJobs, company, apiKey);
 
     let logo =
-      "https://www.ameropa.ro/wp-content/uploads/2021/06/Logo-Ameropa.webp";
+      "https://www.fildas.ro/wp-content/uploads/2023/02/logo-Fildas-894x205.jpg?x32301";
 
     let postLogo = new scraper.ApiScraper(
       "https://api.peviitor.ro/v1/logo/add/"
